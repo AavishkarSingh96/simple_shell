1868921ebd15891f7fe07fe3c5c2cc46d02ee2b5
Shalin-Moodley <shalinkumaranmoodley@gmail.com>
2023-05-24 18:09:57 -0700

    shell

--- getinfo.c
+++ getinfo.c
@@ -1,115 +1,97 @@
 #include "shell.h"
 
 /**
- * wipe - initializes gps_t struct
- * @gps: struct address
+ * clear_info - Initializes the fields of the info_t struct
+ * @info: Pointer to the info_t struct
  */
-void wipe(gps_t *gps)
+void clear_info(info_t *info)
 {
-<<<<<<< HEAD
-	info->arg = NULL;
-	info->argv = NULL;
-	info->path = NULL;
-	info->argc = 0;
-/*Clears the arg, argv, path fields and sets argc to 0*/
-=======
-	gps->arg = NULL;
-	gps->argv = NULL;
-	gps->path = NULL;
-	gps->argc = 0;
->>>>>>> d7febdd3e596410343bf9d607f99b57a080e46b7
+/* Set all fields to initial values or NULL */
+info->arg = NULL;
+info->argv = NULL;
+info->path = NULL;
+info->argc = 0;
 }
-/*this is just context*/
+
 /**
- * build - initializes info_t struct
- * @gps: struct address
- * @court: argument vector
+ * set_info - Sets the fields of the info_t structure
+ * @info: Pointer to the info_t
+ * @av: Argument vector
  */
-void build(gps_t *gps, char **court)
+void set_info(info_t *info, char **av)
 {
-<<<<<<< HEAD
-	int count = 0;
-=======
-	int ac = 0;
->>>>>>> d7febdd3e596410343bf9d607f99b57a080e46b7
+int i = 0;
 
-	gps->fname = court[0];
-	if (gps->arg)
-	{
-		gps->argv = strtow(gmap->arg, " \t");
-		if (!gps->argv)
-		{
-			gps->argv = malloc(sizeof(char *) * 2);
-			if (gps->argv)
-			{
-				gps->argv[0] = _strdup(gps->arg);
-				gps->argv[1] = NULL;
-			}
-		}
-<<<<<<< HEAD
-		for (count = 0; info->argv && info->argv[count]; count++)
-			;
-		info->argc = count;
-		replace_alias(info);
-		replace_vars(info);
-=======
-		for (ac = 0; gps->argv && gps->argv[ac]; ac++)
-			;
-		gps->argc = ac;
-/*this is still section two*/
-		replace_alias(gps);
-		replace_vars(gps);
->>>>>>> d7febdd3e596410343bf9d607f99b57a080e46b7
-	}
-/*Set fname to the first element of av,Split arg into array of strings (argv)*/
-/*Allocate memory for argv with size 2,duplicate arg and store it in argv[0]*/
-/*Set next element of argv to null,Count number of elements in argv (argc)*/
-/*Replace aliases in argv then Replace variables in argv*/
+info->fname = av[0];
 
+/* Check if the argument is provided */
+if (info->arg)
+{
+/* Tokenize the argument string */
+info->argv = strtow(info->arg, " \t");
+if (!info->argv)
+{
+/* Handle memory allocation failure */
+info->argv = malloc(sizeof(char *));
+if (info->argv)
+{
+info->argv[0] = _strdup(info->arg);
+info->argv[1] = NULL;
 }
-/*the end of section two*/
+}
+
+/* Count the number of arguments */
+for (i = 0; info->argv && info->argv[i]; i++)
+;
+info->argc = i;
+
+/* Replace aliases and variables in the arguments */
+replace_alias(info);
+replace_vars(info);
+}
+}
+
 /**
- * buildless - frees gps_t struct fields
- * @gps: struct address
- * @toy: freeing all fields
+ * free_info - Frees the dynamically allocated fields
+ * @info: Pointer to the info_t
+ * @all: True if freeing all fields
  */
-void buildless(gps_t *gps, int toy)
+void free_info(info_t *info, int all)
 {
-	ffree(gps->argv);
-	gps->argv = NULL;
-	gps->path = NULL;
-	if (toy)
-	{
-<<<<<<< HEAD
-		if (!info->cmd_buf)
-			free(info->arg);
-		if (info->env)
-			free_list(&(info->env));
-		if (info->history)
-			free_list(&(info->history));
-		if (info->alias)
-			free_list(&(info->alias));
-		ffree(info->environ);
-		info->environ = NULL;
-		bfree((void **)info->cmd_buf);
-		if (info->readfd > 2)
-			close(info->readfd);
-=======
-		if (!gps->cmd_buf)
-			free(gps->arg);
-		if (gps->env)
-			free_list(&(gps->env));
-		if (gps->history)
-			free_list(&(gps->history));
-		if (gps->alias)
-			free_list(&(gps->alias));
-		ffree(gps->environ);
-			gps->environ = NULL;
-		bfree((void **)gps->cmd_buf);
-		if (gps->readfd > 2)
-			close(gps->readfd);
->>>>>>> d7febdd3e596410343bf9d607f99b57a080e46b7
-		_putchar(BUF_FLUSH);
-	}
-/*Frees memory allocated for argv and sets argv and path to null*/
+ffree(info->argv);
+info->argv = NULL;
+info->path = NULL;
+
+if (all)
+{
+if (!info->cmd_buf)
+free(info->arg);
+/*Free dynamically allocated 'info->arg' if 'info->cmd_buf' is not allocated*/
+
+if (info->env)
+free_list(&(info->env));
+/*Free the linked list in 'info->env' using 'free_list' function*/
+
+if (info->history)
+free_list(&(info->history));
+/*Free the linked list in 'info->history' using 'free_list' function*/
+
+if (info->alias)
+free_list(&(info->alias));
+/*Free the linked list in 'info->alias' using 'free_list' function*/
+
+ffree(info->environ);
+/*Deallocate the memory used by 'info->environ' using 'ffree' function*/
+
+info->environ = NULL;
+/*Set 'info->environ' to NULL after freeing the memory*/
+
+bfree((void **)info->cmd_buf);
+
+if (info->readfd > 2)
+close(info->readfd);
+/*Close the file descriptor if 'info->readfd' is greater than 2*/
+
+_putchar(BUF_FLUSH);
+}
 }
